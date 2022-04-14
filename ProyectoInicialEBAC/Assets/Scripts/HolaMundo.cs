98bebbd72230471f47e504eace7471bfcd70af8c
Daniel <danielcevallos@hotmail.com>
2022-04-13 19:25:21 -0500

    Creación de archivo HolaMundo
    Se creo el script de HolaMundo con ejemplos de Debug

---
+++ ProyectoInicialEBAC/Assets/Scripts/HolaMundo.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HolaMundo : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("normal");
+        Debug.LogError("error");
+        Debug.LogWarning("Warning");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
